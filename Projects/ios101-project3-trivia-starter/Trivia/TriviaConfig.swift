7759890199f04598a5b317fc48e244c86d9604dd
Alexon-Abreu <alexonabreuramirez@gmail.com>
2025-03-11 12:36:10 -0400

    adding trivia configurations

--- Projects/ios101-project3-trivia-starter/Trivia/TriviaConfig.swift
+++ Projects/ios101-project3-trivia-starter/Trivia/TriviaConfig.swift
@@ -5,4 +5,23 @@
 //  Created by Alexon Abreu on 3/9/25.
 //
 
+import Foundation
+import UIKit
+
+
+struct TriviaQuestion {
+    let question: String
+    let correctAnswer: String
+    let wrongAnswers: [String]
+    let category: Category
+
+    var allAnswers: [String] {
+        return [correctAnswer] + wrongAnswers
+    }
+
+    var shuffledAnswers: [String] {
+        return allAnswers.shuffled()
+    }
+}
+
 
