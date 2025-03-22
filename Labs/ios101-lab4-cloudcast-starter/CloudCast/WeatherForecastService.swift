ff15cfcb8fd10de1f59d9214a39410718603c847
Alexon-Abreu <alexonabreuramirez@gmail.com>
2025-03-21 23:24:19 -0400

    transform data to dict

--- Labs/ios101-lab4-cloudcast-starter/CloudCast/WeatherForecastService.swift
+++ Labs/ios101-lab4-cloudcast-starter/CloudCast/WeatherForecastService.swift
@@ -40,6 +40,23 @@ class WeatherForecastService {
   }
     
     
+    private static func parse(data: Data) -> CurrentWeatherForecast {
+        // transform the data we received into a dictionary [String: Any]
+        let jsonDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+        let currentWeather = jsonDictionary["current_weather"] as! [String: Any]
+        // wind speed
+        let windSpeed = currentWeather["windspeed"] as! Double
+        // wind direction
+        let windDirection = currentWeather["winddirection"] as! Double
+        // temperature
+        let temperature = currentWeather["temperature"] as! Double
+        // weather code
+        let weatherCodeRaw = currentWeather["weathercode"] as! Int
+        return CurrentWeatherForecast(windSpeed: windSpeed,
+                                      windDirection: windDirection,
+                                      temperature: temperature,
+                                      weatherCodeRaw: weatherCodeRaw)
+      }
 }
 
 
