6b95cef76682737ea490969618fa1ffe98c3c0ee
Alexon-Abreu <alexonabreuramirez@gmail.com>
2025-04-11 14:00:46 -0400

    added logic to manipulate tasks from local storage

--- Projects/ios101-project7-task-starter/ios101-project7-task/Task.swift
+++ Projects/ios101-project7-task-starter/ios101-project7-task/Task.swift
@@ -5,7 +5,7 @@
 import UIKit
 
 // The Task model
-struct Task {
+struct Task: Codable{
 
     // The task's title
     var title: String
@@ -44,33 +44,46 @@ struct Task {
 
     // The date the task was created
     // This property is set as the current date whenever the task is initially created.
-    let createdDate: Date = Date()
+    var createdDate: Date = Date()
 
     // An id (Universal Unique Identifier) used to identify a task.
-    let id: String = UUID().uuidString
+    var id: String = UUID().uuidString
 }
 
 // MARK: - Task + UserDefaults
 extension Task {
-
+    static let savedKey = "Saved"
 
     // Given an array of tasks, encodes them to data and saves to UserDefaults.
     static func save(_ tasks: [Task]) {
-
-        // TODO: Save the array of tasks
+        let defaults = UserDefaults.standard
+        let encodedData = try! JSONEncoder().encode(tasks)
+        defaults.set(encodedData, forKey: Task.savedKey)
     }
 
     // Retrieve an array of saved tasks from UserDefaults.
     static func getTasks() -> [Task] {
-        
-        // TODO: Get the array of saved tasks from UserDefaults
+        let defaults = UserDefaults.standard
 
-        return [] // 👈 replace with returned saved tasks
+        if let data = defaults.data(forKey: Task.savedKey) {
+            let decodedData = try! JSONDecoder().decode([Task].self, from: data)
+            return decodedData
+        } else {
+            return []
+        }
     }
 
     // Add a new task or update an existing task with the current task.
     func save() {
+        var tasks = Task.getTasks()
 
-        // TODO: Save the current task
+        if let index = tasks.firstIndex(where: { $0.id == self.id }) {
+            tasks.remove(at: index)
+            tasks.insert(self, at: index)
+        } else {
+            tasks.append(self)
+        }
+
+        Task.save(tasks)
     }
 }
