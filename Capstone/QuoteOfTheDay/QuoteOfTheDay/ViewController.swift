860c436f302337a9d6387e3b1d771520e1871f6c
Alexon-Abreu <alexonabreuramirez@gmail.com>
2025-04-17 20:52:48 -0400

    adding capstone prj

---
+++ Capstone/QuoteOfTheDay/QuoteOfTheDay/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  QuoteOfTheDay
+//
+//  Created by Alexon Abreu on 4/17/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var dailyQuote: UILabel!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
